b1c09af9dcffcb77c562fb23dd490c8285001e6d
ldconejo <luisconej@gmail.com>
2015-02-26 12:27:49 -0800

    Finished adding blog code.

---
+++ code/tango_with_django_project/blog/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, url
+from blog import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(
+        r'^view/(?P<slug>[^\.]+).html',
+        'views.view_post',
+        name='view_blog_post'),
+    url(
+        r'^category/(?P<slug>[^\.]+).html',
+        'views.view_category',
+        name='view_blog_category'),
+    )
--- code/tango_with_django_project/tango_with_django_project/urls.py
+++ code/tango_with_django_project/tango_with_django_project/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     #
     url(r'^rango/', include('rango.urls')),
+    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
