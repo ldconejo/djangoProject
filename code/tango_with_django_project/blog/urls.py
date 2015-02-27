8bb90c8cd7bf6618eccebe89631e29d7d3191bbb
ldconejo <luisconej@gmail.com>
2015-02-26 19:56:21 -0800

    Trying fixes to admin interface.

--- code/tango_with_django_project/blog/urls.py
+++ code/tango_with_django_project/blog/urls.py
@@ -3,12 +3,12 @@ from blog import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(
-        r'^view/(?P<slug>[^\.]+).html',
-        'views.view_post',
-        name='view_blog_post'),
     # url(
-    #     r'^category/(?P<slug>[^\.]+).html',
-    #     'views.view_category',
-    #     name='view_blog_category'),
+    #     r'^view/(?P<slug>[^\.]+).html',
+    #     'views.view_post',
+    #     name='view_blog_post'),
+    url(
+        r'^category/(?P<slug>[^\.]+).html',
+        'views.view_category',
+        name='view_blog_category'),
     )
